1396d45da77684c7c6487a8a85c8dd13537009d9
Hirami Takumi <vomqhn2735@gmail.com>
2021-01-26 14:27:21 +0900

    add

--- Example/src/example/Example.java
+++ Example/src/example/Example.java
@@ -24,7 +24,7 @@ public class Example {
         System.out.println("bmakl;");
         System.out.println("name");
         System.out.println("name1");
-        System.out.println("namq");
+        System.out.println("name123");
     }
     
 }
